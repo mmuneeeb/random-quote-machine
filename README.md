95cb2471ba4791a7d657e4fcea239e29e83f8f7e
Muhammad Muneeb <64094867+mmuneeeb@users.noreply.github.com>
2023-08-19 22:38:00 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Random Quote Machine
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/mmuneeeb/pen/ZEqyvZx](https://codepen.io/mmuneeeb/pen/ZEqyvZx).
+
---
+++ dist/script.js
@@ -0,0 +1,68 @@
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}import React from 'https://esm.sh/react@18.2.0';
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0';
+import fontAwesome from "https://cdn.skypack.dev/font-awesome@4.7.0";
+import * as bootstrap from "https://cdn.skypack.dev/bootstrap@5.2.3";
+
+const API = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+
+class App extends React.Component {constructor(...args) {super(...args);_defineProperty(this, "state",
+    {
+      quotes: [{
+        quote: "Life isn’t about getting and having, it’s about giving and being.",
+        author: "Kevin Kruse" }],
+
+      index: 0 });_defineProperty(this, "getRandomIndex",
+
+
+
+
+
+
+
+
+
+
+
+
+    () => {
+      const { quotes, index } = this.state;
+
+      if (quotes.length > 0) {
+        const index = Math.floor(Math.random() * quotes.length);
+        this.setState({
+          index });
+
+      }
+    });}componentDidMount() {//call API and update state
+    fetch(API).then(res => res.json()).then(res => {this.setState({ quotes: res.quotes }, this.GetRandomIndex);});}
+  render() {
+    const { quotes, index } = this.state;
+    const quote = quotes[index];
+    const tweetURL = `http://www.twitter.com/intent/tweet?text=${quote.quote} - ${quote.author}`;
+
+    return /*#__PURE__*/(
+      React.createElement("div", { id: "quote-box", className: "wrapper d-flex align-items-center justify-content-center vh-100" }, /*#__PURE__*/
+      React.createElement("div", { className: "col-6 box p-3 rounded" },
+
+      quote && /*#__PURE__*/
+      React.createElement("div", { className: "mb-4" }, /*#__PURE__*/
+      React.createElement("p", { id: "text" }, /*#__PURE__*/React.createElement("i", { className: "fas fa-quote-left" }), quote.quote), /*#__PURE__*/
+      React.createElement("cite", { id: "author", className: "d-block text-end" }, "- ", quote.author)), /*#__PURE__*/
+
+
+
+      React.createElement("div", { className: "box" }, /*#__PURE__*/
+      React.createElement("div", { className: "d-flex justify-content-between" }, /*#__PURE__*/
+      React.createElement("a", { id: "tweet-quote", href: tweetURL, target: "_blank", className: "btn btn-primary" }, /*#__PURE__*/React.createElement("i", { class: "fab fa-twitter" }), "Tweet"), /*#__PURE__*/
+      React.createElement("button", { id: "new-quote", className: " btn btn-primary", onClick: this.getRandomIndex }, "Get Quote"))))));
+
+
+
+
+
+
+  }}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( /*#__PURE__*/React.createElement(App, null));
